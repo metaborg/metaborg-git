f81c4a8e5e4a740bf510ebbf6026d83b2bede186
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-24 22:49:33 +0200

    Add Kotlin plugin

--- git-library/build.gradle.kts
+++ git-library/build.gradle.kts
@@ -1,8 +1,12 @@
+
+// Workaround for issue: https://youtrack.jetbrains.com/issue/KTIJ-19369
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     `java-library`
     id("org.metaborg.convention.java")
     id("org.metaborg.convention.maven-publish")
     id("org.metaborg.convention.junit")
+    alias(libs.plugins.kotlin.jvm)
 }
 
 publishing {
